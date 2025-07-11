f0e5d5fefbabb4d791bcd530c90e17ff0d136dd1
alittehad <ngogrant454@gmail.com>
2025-07-11 14:00:49 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHGIUaN2JMIXB7HuCM4BjXA97E0bvWYDA",
+  authDomain: "al-ittehad-575a0.firebaseapp.com",
+  databaseURL: "https://al-ittehad-575a0-default-rtdb.firebaseio.com",
+  projectId: "al-ittehad-575a0",
+  storageBucket: "al-ittehad-575a0.appspot.com",
+  messagingSenderId: "797238238041",
+  appId: "1:797238238041:web:89f2aa5c786c59dc374a48"
+};
+firebase.initializeApp(firebaseConfig);
